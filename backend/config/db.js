f4d6c686facb94432d59467d7659141c47350f3f
Prarthana-05 <prarthana.ingawale.st@gmail.com>
2025-08-16 15:48:58 -0700

    db file changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/petAdoptionApp', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // ✅ use env variable
     console.log('MongoDB Connected Successfully');
   } catch (error) {
     console.error('MongoDB Connection Failed', error);
